09a9ef61b7e2a030c64ee98c07d53fe6f92bea7e
asadhijj <hijjawi30@gmail.com>
2022-12-04 07:55:42 +0200

    adding details part

--- snacks/views.py
+++ snacks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView,ListView,DetailView
 from .models import Snack
+
 
 class HomePage(TemplateView):
     template_name='home.html'
@@ -8,3 +9,7 @@ class HomePage(TemplateView):
 class SnackListView(ListView):
     template_name='snack_list.html'
     model=Snack
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
